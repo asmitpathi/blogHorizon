9864010e6a0ee5ff0c995bee3838eb0e64b78e2d
Asmit Pathi <asmitpathi27@gmail.com>
2025-08-25 21:38:08 +0530

    update

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -4,14 +4,12 @@ import appwriteService from "../appwrite/config";
 
 function AllPosts() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {
+    useEffect(() => {}, [])
     appwriteService.getPosts([]).then((posts) => {
         if (posts) {
-            setPosts(posts.documents);
+            setPosts(posts.documents)
         }
-    });
-}, []);
-
+    })
   return (
     <div className='w-full py-8'>
         <Container>
