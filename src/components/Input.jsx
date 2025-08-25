490b52ab12e141c92f17795238d0815cb2beedc8
Asmit Pathi <asmitpathi27@gmail.com>
2025-08-25 19:29:48 +0530

    update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,27 +1,73 @@
-import React, {useId, forwardRef} from 'react'
+import React, { useId, forwardRef, useState } from 'react'
+import Button from './Button'
 
 //one way of using forwardref
-const Input = forwardRef( function Input({
+const Input = forwardRef(function Input({
     label,
     type = "text",
     className = "",
     ...props
-}, ref){
+}, ref) {
     const id = useId()
+    const [fileName, setFileName] = useState("")
+
+    if (type === "file") {
+        return (
+            <div className="w-full">
+                {label && (
+                    <label
+                        className="inline-block mb-1 pl-1"
+                        htmlFor={id}
+                    >
+                        {label}
+                    </label>
+                )}
+
+                {/* hidden file input */}
+                <input
+                    id={id}
+                    type="file"
+                    ref={ref}
+                    {...props}
+                    className="hidden"
+                    onChange={(e) => {
+                        setFileName(e.target.files[0]?.name || "")
+                        props?.onChange?.(e) // still trigger react-hook-form
+                    }}
+                />
+
+                {/* styled button */}
+                <label htmlFor={id}>
+                    <Button type="button" className="cursor-pointer">
+                        Choose File
+                    </Button>
+                </label>
+
+                {/* show chosen file name if available */}
+                {fileName && (
+                    <span className="ml-2 text-sm text-gray-700">{fileName}</span>
+                )}
+            </div>
+        )
+    }
+
+    // default input (non-file types)
     return (
-        <div className='w-full'>
-            {label && <label 
-            className='inline-block mb-1 pl-1' 
-            htmlFor={id}>
-                {label}
-            </label>
-            }
+        <div className="w-full">
+            {label && (
+                <label
+                    className="inline-block mb-1 pl-1"
+                    htmlFor={id}
+                >
+                    {label}
+                </label>
+            )}
             <input
-            type={type}
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            ref={ref}
-            {...props}
-            id={id}
+                type={type}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
             />
         </div>
     )
